3214f9f5abe62b3c1584038a724d3e6b099ba9df
minhduc <ducbv1407@gmail.com>
2026-02-26 19:12:55 +0100

    chore: use TIME_UNIT constants instead of plain strings

--- core/src/date/adapter.ts
+++ core/src/date/adapter.ts
@@ -1,4 +1,4 @@
-import type { DateAdapter } from "./types"
+import {DateAdapter, TIME_UNIT} from "./types"
 
 /**
  * Default implementation of the DateAdapter interface using native JavaScript Date objects.
@@ -23,32 +23,41 @@ export const nativeDateAdapter: DateAdapter = {
     diff(a, b, unit) {
         const ms = b.getTime() - a.getTime()
         switch (unit) {
-            case 'second': return ms / 1000
-            case 'minute': return ms / (1000 * 60)
-            case 'hour': return ms / (1000 * 60 * 60)
-            case 'day': return ms / (1000 * 60 * 60 * 24)
-            default: return ms
+            case TIME_UNIT.SECOND: return ms / 1000
+            case TIME_UNIT.MINUTE: return ms / (1000 * 60)
+            case TIME_UNIT.HOUR: return ms / (1000 * 60 * 60)
+            case TIME_UNIT.DAY: return ms / (1000 * 60 * 60 * 24)
+            case TIME_UNIT.WEEK: return ms / (1000 * 60 * 60 * 24 * 7)
+            case TIME_UNIT.MONTH: {
+                const years = b.getFullYear() - a.getFullYear()
+                const months = b.getMonth() - a.getMonth()
+                return years * 12 + months
+            }
+            case TIME_UNIT.YEAR:
+                return b.getFullYear() - a.getFullYear()
+            default:
+                return ms
         }
     },
     startOf(date, unit) {
         const out = new Date(date)
-        if (unit === 'day' || unit === 'hour' || unit === 'minute' || unit === 'second') {
+        if (unit === TIME_UNIT.DAY || unit === TIME_UNIT.HOUR || unit === TIME_UNIT.MINUTE || unit === TIME_UNIT.SECOND) {
             out.setSeconds(0, 0)
         }
-        if (unit === 'day' || unit === 'hour' || unit === 'minute') out.setMinutes(0)
-        if (unit === 'day' || unit === 'hour') out.setHours(0)
-        if (unit === 'day') out.setHours(0)
-        if (unit === 'week') {
+        if (unit === TIME_UNIT.DAY || unit === TIME_UNIT.HOUR || unit === TIME_UNIT.MINUTE) out.setMinutes(0)
+        if (unit === TIME_UNIT.DAY || unit === TIME_UNIT.HOUR) out.setHours(0)
+        if (unit === TIME_UNIT.DAY) out.setHours(0)
+        if (unit === TIME_UNIT.WEEK) {
             out.setHours(0, 0, 0, 0)
             const d = out.getDay()
             const diff = out.getDate() - d + (d === 0 ? -6 : 1)
             out.setDate(diff)
         }
-        if (unit === 'month') {
+        if (unit === TIME_UNIT.MONTH) {
             out.setDate(1)
             out.setHours(0, 0, 0, 0)
         }
-        if (unit === 'year') {
+        if (unit === TIME_UNIT.YEAR) {
             out.setMonth(0, 1)
             out.setHours(0, 0, 0, 0)
         }
@@ -57,10 +66,10 @@ export const nativeDateAdapter: DateAdapter = {
     endOf(date, unit) {
         const start = this.startOf(date, unit)
         switch (unit) {
-            case 'day': return this.add(start, { days: 1 })
-            case 'week': return this.add(start, { weeks: 1 })
-            case 'month': return this.add(start, { months: 1 })
-            case 'year': return this.add(start, { years: 1 })
+            case TIME_UNIT.DAY: return this.add(start, { days: 1 })
+            case TIME_UNIT.WEEK: return this.add(start, { weeks: 1 })
+            case TIME_UNIT.MONTH: return this.add(start, { months: 1 })
+            case TIME_UNIT.YEAR: return this.add(start, { years: 1 })
             default: return start
         }
     },
